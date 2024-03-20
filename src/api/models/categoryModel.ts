1207604d3d0f838aa4d14d6997f2812cb1b9334d
Liideli <roope.kasperi.laine@gmail.com>
2024-03-20 17:11:23 +0200

    get by id done

--- src/api/models/categoryModel.ts
+++ src/api/models/categoryModel.ts
@@ -1,7 +1,7 @@
+import CustomError from '../../classes/CustomError';
 import promisePool from '../../database/db';
 import {Category} from '../../types/DBTypes';
 import {RowDataPacket} from 'mysql2';
-
 const getAllCategories = async () => {
   const [rows] = await promisePool.execute<RowDataPacket[] & Category[]>(
     'SELECT * FROM categories'
@@ -13,14 +13,16 @@ const getAllCategories = async () => {
 };
 
 const getCategoryById = async (id: number) => {
-  const [rows] = await promisePool.execute<RowDataPacket[] & Category[]>(
+  const sql = promisePool.format(
     'SELECT * FROM categories WHERE category_id = ?',
     [id]
   );
-  if (!rows) {
-    throw new Error('No categories found');
+  console.log(sql);
+  const [rows] = await promisePool.execute<RowDataPacket[] & Category[]>(sql);
+  if (rows.length === 0) {
+    throw new CustomError('No categories found', 404);
   }
-  return rows[0] as Category;
+  return rows[0];
 };
 
 export {getAllCategories, getCategoryById};
