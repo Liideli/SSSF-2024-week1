7dd24e2f1adb54b1fe22007faba9defc339669f8
Liideli <roope.kasperi.laine@gmail.com>
2024-03-21 11:15:45 +0200

    put category

--- src/api/routes/categoryRoute.ts
+++ src/api/routes/categoryRoute.ts
@@ -4,7 +4,8 @@ import {
   categoryListGet,
   categoryPost,
 } from '../controllers/categoryController';
-import {body} from 'express-validator';
+import {body, param} from 'express-validator';
+import {validationErrors} from '../../middlewares';
 
 const router = express.Router();
 
@@ -13,8 +14,22 @@ router.route('/').get(categoryListGet);
 router
   .route('/')
   .get(categoryListGet)
-  .post(body('category_name').notEmpty().isString().escape(), categoryPost);
+  .post(
+    body('category_name').notEmpty().isString().isAlpha().escape(),
+    validationErrors,
+    categoryPost
+  );
 
-router.route('/:id').get(categoryGet);
+router.route('/:id').get(param('id').isInt(), validationErrors, categoryGet);
+
+router
+  .route('/:id')
+  .get(param('id').isInt(), validationErrors, categoryGet)
+  .put(
+    param('id').isInt(),
+    body('category_name').notEmpty().isString().isAlpha().escape(),
+    validationErrors,
+    categoryGet
+  );
 
 export default router;
